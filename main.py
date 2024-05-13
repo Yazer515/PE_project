5386cf6fe5a28e64ec4c05e66dd349fc8be982fc
Yazer515 <yaroslav515@mail.ru>
2024-05-13 20:44:40 +0300

    new file:   dockerfile modified:   main.py new file:   requirements.txt

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print("Hello world!")
-print("It's the project for PE")
+import telebot
+
+
+bot = telebot.TeleBot('6204926151:AAH_u-Fjhs0C-NcX3kwaBdMps8I9C65fzv0')
+    
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
