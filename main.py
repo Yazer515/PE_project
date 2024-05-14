35c93eea018a6724a082b95ac051207c88726dd0
Yazer515 <yaroslav515@mail.ru>
2024-05-14 10:39:10 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print("Hello world!")
-print("It's the project for PE")
+import os
+from dotenv import load_dotenv
+
+import telebot
+
+load_dotenv()
+
+key = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(key)
+    
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        
+if name == "main":
+    bot.polling(none_stop=True, interval=0)
