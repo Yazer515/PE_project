a4d6f8a96ee3dbfb4231e296c71a9c2c9950ca74
Yazer515 <yaroslav515@mail.ru>
2024-05-14 10:53:36 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет")
+    elif message.text == "/start":
+        bot.send_message(message.from_user.id, "Напиши /help")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
         
