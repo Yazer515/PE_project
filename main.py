69addf78f1949fae151d728efe089b54ba2f9b46
Yazer515 <yaroslav515@mail.ru>
2024-05-14 11:04:46 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ load_dotenv()
 key = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(key)
-    
-
+  
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "Привет":
@@ -21,5 +20,5 @@ def get_text_messages(message):
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
         
-if __name__ == "main":
+if __name__ == "__main__":
     bot.polling(none_stop=True, interval=0)
