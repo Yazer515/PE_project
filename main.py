008be2cf11363427d78d2f184de993f7db9c9625
Yazer515 <yaroslav515@mail.ru>
2024-05-15 09:34:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
 import telebot
-
-load_dotenv()
 
 key = os.environ["BOT_TOKEN"]
 
@@ -11,7 +8,7 @@ bot = telebot.TeleBot(key)
   
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == "Привет":
+    if message.text == "привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет")
